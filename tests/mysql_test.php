dc34a2caa4f148819d7fb86c017f925fec2a71d0
Mayumi06 <juliaayyumii@gmail.com>
2022-08-10 12:24:28 -0300

    prw

---
+++ tests/mysql_test.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../includes/funcoes.php';
+require_once '../core/conexao_mysql.php';
+require_once '../core/sql.php';
+require_once '../core/mysql.php';
+
+insert_teste('Julia', 'juliamayumi@outlook.com','1234567');
+buscar_teste();
+update_teste(38, 'Mayumi','mayumi@gmailcom');
+buscar_teste();
+
+function insert_teste($nome, $email, $senha) : void
+{
+    $dados = ['nome' => $nome, 'email' => $email, 'senha' => $senha];
+    insere('usuario',$dados);
+}
+
+function buscar_teste() : void
+{
+    $usuarios = buscar('usuario',['id','nome','email'],[],'');
+    print_r($usuarios);
+}
+
+function update_teste($id, $nome, $email) : void
+{
+    $dados = ['nome' => $nome, 'email' => $email];
+    $criterio = [['id', '=', $id]];
+    atualiza('usuario',$dados,$criterio);
+}
+?>
